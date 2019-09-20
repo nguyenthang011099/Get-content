ab6275e43cfc05ee06bd9bf49137f634bc585b0f
thang <nguyenthang011099.hust@gmail.com>
2019-09-20 16:19:20 +0700

    update file beaty

--- crawd_weather.php
+++ crawd_weather.php
@@ -1,31 +1,44 @@
 <?php
-//require"db.php";
+
 include("simple_html_dom.php");
+
+//
+//$html= file_get_html("https://www.accuweather.com/en/vn/hanoi/353412/hourly-weather-forecast/353412");
+//
+//echo $html;
+
 $html=file_get_html("http://www.nchmf.gov.vn/web/vi-VN/62/19/58/map/Default.aspx");
 
-
-$t3=$html->find("td.forecast_detail",0);
-$t4=$t3->find("strong",0);
-$t5=$t4->innertext;
-$t6=preg_replace('/\D/',"",$t5 );
-echo $t6."\n";
-
-$t3=$html->find("td.forecast_detail",1);
-$t4=$t3->find("strong",0);
-echo $t4->innertext;
-
-$t3=$html->find("td.forecast_detail",2);
-$t4=$t3->find("strong",0);
-$t5=$t4->innertext;
-$t6=preg_replace('/\D/',"",$t5 );
-echo $t6."\n";
+//get temprature
+$t1=$html->find("td.forecast_detail",0);
+$t2=$t1->find("strong",0);
+$t3=$t2->innertext;
+// t4 is temprature
+$t4=preg_replace('/\D/',"",$t3 );
 
 
-$t3=$html->find("td.forecast_detail",3);
-$t4=$t3->find("strong",0);
-$t5= $t4->innertext;
-$t6=preg_replace('/\D/',"",$t5 );
-echo $t6."\n";
+
+//get description
+$d1=$html->find("td.forecast_detail",1);
+$d2=$d1->find("strong",0);
+$d3=$d2->innertext;
+
+
+//get humid
+$h1=$html->find("td.forecast_detail",2);
+$h2=$h1->find("strong",0);
+$h3=$h2->innertext;
+$h4=preg_replace('/\D/',"",$h3 );
+
+
+// get V of wind
+$w1=$html->find("td.forecast_detail",3);
+$w2=$w1->find("strong",0);
+$w3= $w2->innertext;
+$w4=preg_replace('/\D/',"",$w3 );
+
+
+echo $w4;
 
 
 
--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect("localhost","root","","");
-mysql_select_db("dantri");
-mysql_query("SET NAMES 'utf8'");
