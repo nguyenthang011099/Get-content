02a7ec19b6d76a27f7d5cf20164d2363e7501dbb
Nguyen Thang <52337737+nguyenthang011099@users.noreply.github.com>
2019-09-25 14:17:38 +0700

    Add files via upload
    update chose website on terminal

--- dbCon.php
+++ dbCon.php
@@ -1,11 +1,13 @@
 <?php
 
-$dbServername="localhost";
-$dbUsername="root";
-$dbPassword="";
-$dbName="weather";
-$conn= mysql_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
+    $dbServername="localhost";
+    $dbUsername="root";
+    $dbPassword="";
+    $dbName="weather";
 
-
-?>
+$conn = new mysqli( $dbServername, $dbUsername, $dbPassword, $dbName );
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
---
+++ govweather.php
@@ -0,0 +1,47 @@
+
+<?php
+    include("simple_html_dom.php");
+    //require_once "dbCon.php";
+
+
+    if (isset($argc)) {
+        for ($i = 2; $i < $argc; $i++) {
+
+            $html=file_get_html("http://www.nchmf.gov.vn/web/vi-VN/62/19/58/map/Default.aspx");
+            //get temprature
+            $t1=$html->find("td.forecast_detail",0);
+            $t2=$t1->find("strong",0);
+            $t3=$t2->innertext;
+            // t4 is temprature
+            $t4=preg_replace('/\D/',"",$t3 );
+            //get description
+            $d1=$html->find("td.forecast_detail",1);
+            $d2=$d1->find("strong",0);
+            $d3=$d2->innertext;
+            //get humid
+            $h1=$html->find("td.forecast_detail",2);
+            $h2=$h1->find("strong",0);
+            $h3=$h2->innertext;
+            $h4=preg_replace('/\D/',"",$h3 );
+            // get V of wind
+            $w1=$html->find("td.forecast_detail",3);
+            $w2=$w1->find("strong",0);
+            $w3= $w2->innertext;
+            $w4=preg_replace('/\D/',"",$w3 );
+
+            echo $t4;
+
+            //query data
+    //    $qr= "INSERT INTO hanoi (Temperature, Humid, Wind, Description )
+    //    VALUES ('$t4','$h4','$w4','$d3')";
+    //    mysqli_query($qr);
+    //    mysqli_close($conn);
+        }
+    }
+    else {
+        echo "argc and argv disabled\n";
+    }
+
+
+?>
+
