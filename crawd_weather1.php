e1d9e5280356fcf1e05768e253d28c18a31731bb
Nguyen Thang <52337737+nguyenthang011099@users.noreply.github.com>
2019-10-03 16:36:15 +0700

    add get humid

--- crawd_weather1.php
+++ crawd_weather1.php
@@ -14,4 +14,8 @@ $w=$html->find('text.wind-icon-val',0);
 $w1=preg_replace('/\D/',"",$w);
 $w2=substr($w1,0,2);
 
+$h=$html->find('tr.b-forecast__table-humidity',0);
+$h1=$h->find('span.b-forecast__table-value',0);
+$h2=preg_replace('/\D/',"",$h1);
+
 
