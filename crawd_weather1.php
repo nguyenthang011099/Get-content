f669fbbacd5f8e4af3cd4aaaff4a241ac4e4879d
Nguyen Thang <52337737+nguyenthang011099@users.noreply.github.com>
2019-10-03 16:28:51 +0700

    add code get v of wind

--- crawd_weather1.php
+++ crawd_weather1.php
@@ -1,7 +1,17 @@
 
 <?php
 include('simple_html_dom.php');
+
 $html=file_get_html('https://www.weather-forecast.com/locations/Hanoi/forecasts/latest');
+
+
 $t= $html->find('td.temp-color1',0);
 $t1= $t->innertext();
 $t2=preg_replace('/\D/',"",$t1 );
+
+
+$w=$html->find('text.wind-icon-val',0);
+$w1=preg_replace('/\D/',"",$w);
+$w2=substr($w1,0,2);
+
+
