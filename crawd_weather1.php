e30b4d28002398625b846eba42646fec52a3f1aa
Nguyen Thang <52337737+nguyenthang011099@users.noreply.github.com>
2019-10-03 15:57:43 +0700

    get temperature from weather forcast

--- crawd_weather1.php
+++ crawd_weather1.php
@@ -2,3 +2,6 @@
 <?php
 include('simple_html_dom.php');
 $html=file_get_html('https://www.weather-forecast.com/locations/Hanoi/forecasts/latest');
+$t= $html->find('td.temp-color1',0);
+$t1= $t->innertext();
+$t2=preg_replace('/\D/',"",$t1 );
